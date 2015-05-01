f2ca7e46d5326bd8fc25d5cfe05a641072228f96
Niole Nelson <niolenelson@gmail.com>
2015-05-01 14:55:11 -0400

    New initial push, reset commit history.

--- frontend/App.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*global React*/
-
-var React = require('react');
-var LoginCreateUser = require('./LoginCreateUser.jsx');
-
-var App = React.render( <LoginCreateUser/>, $('#container')[0]);
-
-module.exports = App;
